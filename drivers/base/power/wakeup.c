b4083bb58f8e635ef9cb88efa354c3479e5e47a3
fezzy102 <fezzy102@gmail.com>
2017-04-11 16:19:28 -0400

    Fix build this is defined twice.

--- drivers/base/power/wakeup.c
+++ drivers/base/power/wakeup.c
@@ -40,9 +40,6 @@ module_param(enable_timerfd_ws, bool, 0644);
 static bool enable_netlink_ws = true;
 module_param(enable_netlink_ws, bool, 0644);
 
-static bool enable_bluedroid_timer_ws = true;
-module_param(enable_bluedroid_timer_ws, bool, 0644);
-
 /*
  * If set, the suspend/hibernate code will abort transitions to a sleep state
  * if wakeup events are registered during or immediately before the transition.
